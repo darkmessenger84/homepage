ac0f69d0a3a035b4f96543091066226a1757efb4
darkmessenger84 <davep.johnson1984@gmail.com>
2019-11-18 20:27:51 +0000

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -2,21 +2,18 @@ var xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = function() {
   
-  console.log("State change")
   if (this.readyState == 4 
   &&  this.status == 200)
   {
-    console.log("Ready")
     document.getElementById("contentwrapper").innerHTML = this.responseText;
   }
-
+  
 }
 
 function pull(res)
 {
-  console.log("Called func")
+  
   xhr.open('GET', res);
-  console.log(res)
   xhr.send();
-
+  
 }
